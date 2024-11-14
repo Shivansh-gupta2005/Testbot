b7ea0d421d78d036fe079e56b47cbace4ba8c1f5
Shivansh-gupta2005 <147073873+Shivansh-gupta2005@users.noreply.github.com>
2024-11-14 18:48:16 +0530

    Update testbot_with_esp32.ino

--- testbot_with_esp32.ino
+++ testbot_with_esp32.ino
@@ -3,27 +3,19 @@
 
 // Motor pin definitions
 #define DIR1 2   //8
-#define PWM1 0   //9
+#define PWM1 18  //9
 #define DIR2 4   //6
 #define PWM2 5   //7
-#define DIR3 16  //15
+#define DIR3 19 //15
 #define PWM3 14  //14
-#define DIR4 17  //22
+#define DIR4 27  //22
 #define PWM4 15  //20
 
 // WiFi credentials - replace with your network details
-const char* ssid = "Nokia G42 5G;
+const char* ssid = "Nokia G42 5G";
 const char* password = "shivansh2005";
 
 WebServer server(80);
-
-// PWM properties
-const int freq = 5000;
-const int resolution = 8;
-const int pwmChannel1 = 0;
-const int pwmChannel2 = 1;
-const int pwmChannel3 = 2;
-const int pwmChannel4 = 3;
 
 // HTML webpage
 const char index_html[] PROGMEM = R"rawliteral(
@@ -91,33 +83,25 @@ const char index_html[] PROGMEM = R"rawliteral(
 void setup() {
     Serial.begin(115200);
     
-    // Configure PWM
-    ledcSetup(pwmChannel1, freq, resolution);
-    ledcSetup(pwmChannel2, freq, resolution);
-    ledcSetup(pwmChannel3, freq, resolution);
-    ledcSetup(pwmChannel4, freq, resolution);
-    
-    // Attach PWM channels to pins
-    ledcAttachPin(PWM1, pwmChannel1);
-    ledcAttachPin(PWM2, pwmChannel2);
-    ledcAttachPin(PWM3, pwmChannel3);
-    ledcAttachPin(PWM4, pwmChannel4);
-    
     // Set pin modes
     pinMode(DIR1, OUTPUT);
     pinMode(DIR2, OUTPUT);
     pinMode(DIR3, OUTPUT);
     pinMode(DIR4, OUTPUT);
+    pinMode(PWM1, OUTPUT);
+    pinMode(PWM2, OUTPUT);
+    pinMode(PWM3, OUTPUT);
+    pinMode(PWM4, OUTPUT);
     
     // Initialize all pins to LOW
     digitalWrite(DIR1, LOW);
     digitalWrite(DIR2, LOW);
     digitalWrite(DIR3, LOW);
     digitalWrite(DIR4, LOW);
-    ledcWrite(pwmChannel1, 0);
-    ledcWrite(pwmChannel2, 0);
-    ledcWrite(pwmChannel3, 0);
-    ledcWrite(pwmChannel4, 0);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 0);
+    analogWrite(PWM3, 0);
+    analogWrite(PWM4, 0);
     
     // Connect to WiFi
     WiFi.begin(ssid, password);
@@ -154,10 +138,10 @@ void moveForward() {
     digitalWrite(DIR2, LOW);
     digitalWrite(DIR3, LOW);
     digitalWrite(DIR4, LOW);
-    ledcWrite(pwmChannel1, 255);  // Changed from 1024 to 255 for 8-bit resolution
-    ledcWrite(pwmChannel2, 0);
-    ledcWrite(pwmChannel3, 255);
-    ledcWrite(pwmChannel4, 0);
+    analogWrite(PWM1, 255);
+    analogWrite(PWM2, 0);
+    analogWrite(PWM3, 255);
+    analogWrite(PWM4, 0);
     server.send(200, "text/plain", "Moving forward");
 }
 
@@ -166,10 +150,10 @@ void moveLeft() {
     digitalWrite(DIR2, HIGH);
     digitalWrite(DIR3, LOW);
     digitalWrite(DIR4, HIGH);
-    ledcWrite(pwmChannel1, 0);
-    ledcWrite(pwmChannel2, 255);
-    ledcWrite(pwmChannel3, 0);
-    ledcWrite(pwmChannel4, 255);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 255);
+    analogWrite(PWM3, 0);
+    analogWrite(PWM4, 255);
     server.send(200, "text/plain", "Moving left");
 }
 
@@ -178,10 +162,10 @@ void moveBackward() {
     digitalWrite(DIR2, LOW);
     digitalWrite(DIR3, HIGH);
     digitalWrite(DIR4, LOW);
-    ledcWrite(pwmChannel1, 255);
-    ledcWrite(pwmChannel2, 0);
-    ledcWrite(pwmChannel3, 255);
-    ledcWrite(pwmChannel4, 0);
+    analogWrite(PWM1, 255);
+    analogWrite(PWM2, 0);
+    analogWrite(PWM3, 255);
+    analogWrite(PWM4, 0);
     server.send(200, "text/plain", "Moving backward");
 }
 
@@ -190,10 +174,10 @@ void moveRight() {
     digitalWrite(DIR2, LOW);
     digitalWrite(DIR3, LOW);
     digitalWrite(DIR4, LOW);
-    ledcWrite(pwmChannel1, 0);
-    ledcWrite(pwmChannel2, 255);
-    ledcWrite(pwmChannel3, 0);
-    ledcWrite(pwmChannel4, 255);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 255);
+    analogWrite(PWM3, 0);
+    analogWrite(PWM4, 255);
     server.send(200, "text/plain", "Moving right");
 }
 
@@ -202,10 +186,10 @@ void spinLeft() {
     digitalWrite(DIR2, HIGH);
     digitalWrite(DIR3, LOW);
     digitalWrite(DIR4, LOW);
-    ledcWrite(pwmChannel1, 255);
-    ledcWrite(pwmChannel2, 255);
-    ledcWrite(pwmChannel3, 255);
-    ledcWrite(pwmChannel4, 255);
+    analogWrite(PWM1, 255);
+    analogWrite(PWM2, 255);
+    analogWrite(PWM3, 255);
+    analogWrite(PWM4, 255);
     server.send(200, "text/plain", "Spinning left");
 }
 
@@ -214,17 +198,17 @@ void spinRight() {
     digitalWrite(DIR2, LOW);
     digitalWrite(DIR3, HIGH);
     digitalWrite(DIR4, HIGH);
-    ledcWrite(pwmChannel1, 255);
-    ledcWrite(pwmChannel2, 255);
-    ledcWrite(pwmChannel3, 255);
-    ledcWrite(pwmChannel4, 255);
+    analogWrite(PWM1, 255);
+    analogWrite(PWM2, 255);
+    analogWrite(PWM3, 255);
+    analogWrite(PWM4, 255);
     server.send(200, "text/plain", "Spinning right");
 }
 
 void stopMotors() {
-    ledcWrite(pwmChannel1, 0);
-    ledcWrite(pwmChannel2, 0);
-    ledcWrite(pwmChannel3, 0);
-    ledcWrite(pwmChannel4, 0);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 0);
+    analogWrite(PWM3, 0);
+    analogWrite(PWM4, 0);
     server.send(200, "text/plain", "Stopped");
 }
